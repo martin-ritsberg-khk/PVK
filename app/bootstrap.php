2ff34d2456c2dfdd4133e79aed50a8c22f9d455f
martin-ritsberg-khk <martin.ritsberg@khk.ee>
2020-01-27 12:54:38 +0200

    Puhastus 2

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -6,7 +6,5 @@ spl_autoload_register( function ($className){
 
 //load required libraries
 //require_once 'libraries/Core.php';
-//require_once 'libraries/Text.php';
-//require_once 'libraries/ColorText.php';
 //require_once 'libraries/Controller.php';
 require_once 'config/constants.php';

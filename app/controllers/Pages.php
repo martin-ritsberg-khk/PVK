0eed341a5aead6b26f8fa15a59c0d5baf9a8ab6d
martin-ritsberg-khk <martin.ritsberg@khk.ee>
2020-01-27 13:21:02 +0200

    Puhastus 3

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -8,26 +8,6 @@ class Pages extends Controller
      */
     public function __construct()
     {
-        $this->pagesModel = $this->model('Page');
-    }
-
-    public function test(){
-        $users = $this->pagesModel->getUsers();
-        print_r($users);
-    }
-
-    public function test2(){
-        $users = $this->pagesModel->getOneUser(2);
-        print_r($users);
-    }
-
-    public function test3(){
-        $users = $this->pagesModel->usersCount();
-        print_r($users);
-    }
-
-    public function edit(){
-        $this->view('pages/edit');
     }
 
     public function index() {
